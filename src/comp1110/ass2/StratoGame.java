88186a253ff9452256a764b0e1d4698817611c4e
COMP1110 Sem 2 2016 <steve.blackburn+comp1110-2016@anu.edu.au>
2016-08-09 12:47:52 +1000

    Initial import

---
+++ src/comp1110/ass2/StratoGame.java
@@ -0,0 +1,84 @@
+package comp1110.ass2;
+
+/**
+ * This class provides the text interface for the Strato Game
+ *
+ * The game is based directly on Gigamic's Stratopolis game
+ * (http://boardgamegeek.com/boardgame/125022/stratopolis)
+ */
+public class StratoGame {
+
+    /**
+     * Determine whether a tile placement is well-formed according to the following:
+     * - it consists of exactly four characters
+     * - the first character is in the range A .. Z
+     * - the second character is in the range A .. Z
+     * - the third character is in the range A .. U
+     * - the fourth character is in the range A .. D
+     *
+     * @param tilePlacement A string describing a tile placement
+     * @return True if the tile placement is well-formed
+     */
+    static boolean isTilePlacementWellFormed(String tilePlacement) {
+        // FIXME Task 3: determine whether a tile placement is well-formed
+        return false;
+    }
+
+    /**
+     * Determine whether a placement string is well-formed:
+     *  - it consists of exactly N four-character tile placements (where N = 1 .. 41)
+     *  - each tile placement is well-formed
+     *  - the first tile placement is 'MMUA'
+     *  - the second tile placement (if any) is for a green tile
+     *  - remaining tile placements alternate between red and green
+     *  - no tile appears more than twice in the placement
+     *
+     * @param placement A string describing a placement of one or more tiles
+     * @return True if the placement is well-formed
+     */
+    static boolean isPlacementWellFormed(String placement) {
+        // FIXME Task 4: determine whether a placement is well-formed
+        String foo;
+        return false;
+    }
+
+    /**
+     * Determine whether a placement is valid.  To be valid, the placement must be well-formed
+     * and each tile placement must follow the game's placement rules.
+     *
+     * @param placement A placement string
+     * @return True if the placement is valid
+     */
+    static boolean isPlacementValid(String placement) {
+        // FIXME Task 6: determine whether a placement is valid
+        return false;
+    }
+
+    /**
+     * Determine the score for a player given a placement, following the
+     * scoring rules for the game.
+     *
+     * @param placement A placement string
+     * @param green True if the score for the green player is requested,
+     *              otherwise the score for the red player should be returned
+     * @return the score for the requested player, given the placement
+     */
+    static int getScoreForPlacement(String placement, boolean green) {
+        // FIXME Task 7: determine the score for a player given a placement
+        return 0;
+    }
+
+    /**
+     * Generate a valid move that follows from: the given placement, a piece to
+     * play, and the piece the opponent will play next.
+     *
+     * @param placement  A valid placement string indicating a game state
+     * @param piece  The piece you are to play ('A' to 'T')
+     * @param opponentsPiece The piece your opponent will be asked to play next ('A' to 'T' or 0 if last move).
+     * @return A string indicating a valid tile placement that represents your move.
+     */
+    static String generateMove(String placement, char piece, char opponentsPiece) {
+        // FIXME Task 10: generate a valid move
+        return null;
+    }
+}
---
+++ src/comp1110/ass2/gui/Board.java
@@ -0,0 +1,22 @@
+package comp1110.ass2.gui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Board extends Application {
+    private static final int BOARD_WIDTH = 933;
+    private static final int BOARD_HEIGHT = 700;
+
+    // FIXME Task 8: Implement a basic playable Strato Game in JavaFX that only allows pieces to be placed in valid places
+
+    // FIXME Task 9: Implement scoring
+
+    // FIXME Task 11: Implement a game that can play valid moves (even if they are weak moves)
+
+    // FIXME Task 12: Implement a game that can play good moves
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
